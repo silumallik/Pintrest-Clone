633996559c15fa4cd99f6a6b62a22550712b599b
Silu Mallik <silumallik402@gmail.com>
2026-04-17 17:09:07 +0530

    Remove console log from GET session handler
    Remove console log of session in GET function.

--- src/app/api/session/route.js
+++ src/app/api/session/route.js
@@ -3,7 +3,6 @@ import { authOptions } from "@/lib/authOptions";
 
 export async function GET(req) {
   const session = await getServerSession(authOptions);
-  console.log(session);
   
   return new Response(JSON.stringify({ session }));
 }
