1ddfeff729b4ba82d9e3477e8bac8e0680701279
Silu Mallik <silumallik402@gmail.com>
2026-04-17 12:35:17 +0530

    Update sessionToken secure option based on environment

--- src/lib/authOptions.js
+++ src/lib/authOptions.js
@@ -162,7 +162,7 @@ export const authOptions = {
   cookies: {
     sessionToken: {
       name: "next-auth.session-token",
-      options: { httpOnly: true, sameSite: "lax", path: "/", secure: false },
+      options: { httpOnly: true, sameSite: "lax", path: "/", secure: process.env.NODE_ENV === "production" },
     },
   },
 
