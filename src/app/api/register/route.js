267f75471391df4df9aedf7454e95237b46d9cc0
Silu Mallik <silumallik402@gmail.com>
2026-04-17 17:06:58 +0530

    Fix username generation and clean up commented code

--- src/app/api/register/route.js
+++ src/app/api/register/route.js
@@ -37,7 +37,7 @@ export async function POST(req) {
       name,
       email,
       password: hashedPassword,
-      username: email.split("@")[0] + Date.now(),         //slove error
+      username: email.split("@")[0] + Date.now(),         
     });
 
     return NextResponse.json(
@@ -45,7 +45,6 @@ export async function POST(req) {
       { status: 201 }
     );
   } catch (error) {
-    console.log("FULL ERROR:", error);
     return NextResponse.json(
       { message: "Server error" },
       { status: 500 }
@@ -53,62 +52,3 @@ export async function POST(req) {
   }
 }
 
-
-
-
-
-// import { NextResponse } from "next/server";
-// import connectDB from "@/lib/db";
-// import User from "@/models/User";
-// import bcrypt from "bcryptjs";
-
-// export async function POST(req) {
-//   try {
-//     await connectDB();
-
-//     const body = await req.json();
-//     const { name, email, password } = body;
-
-//     // Validation
-//     if (!name || !email || !password) {
-//       return NextResponse.json(
-//         { message: "All fields are required" },
-//         { status: 400 }
-//       );
-//     }
-
-//     // Check existing user
-//     const existingUser = await User.findOne({
-//       email: email.toLowerCase(),
-//     });
-
-//     if (existingUser) {
-//       return NextResponse.json(
-//         { message: "User already exists" },
-//         { status: 400 }
-//       );
-//     }
-
-//     // Hash password
-//     const hashedPassword = await bcrypt.hash(password, 10);
-
-//     // Create user
-//     await User.create({
-//       name,
-//       email: email.toLowerCase(),
-//       password: hashedPassword,
-//     });
-
-//     return NextResponse.json(
-//       { message: "User registered successfully" },
-//       { status: 201 }
-//     );
-
-//   } catch (error) {
-//     console.log("REGISTER ERROR:", error);
-//     return NextResponse.json(
-//       { message: "Internal Server Error" },
-//       { status: 500 }
-//     );
-//   }
-// }
